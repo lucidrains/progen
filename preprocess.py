ee25010fcb63500f0ad612e4c2fc02072ec3aa6c
Phil Wang <lucidrains@gmail.com>
2021-06-24 15:16:04 -0700

    clickify preprocess file

--- preprocess.py
+++ preprocess.py
@@ -1,19 +1,29 @@
 import os
 import gzip
+import click
 from itertools import islice
 from Bio import SeqIO
 
-os.makedirs('./data', exist_ok = True)
+@click.command()
+@click.option('--read_from', default = './uniref50.fasta')
+@click.option('--write_to', default = './data/uniref50.sample.gz')
+@click.option('--num_samples', default = 25000)
+@click.option('--max_seq_len', default = 2048)
+def main(
+    read_from,
+    write_to,
+    num_samples,
+    max_seq_len
+):
+    it = SeqIO.parse(read_from, 'fasta')
+    it = filter(lambda t: len(t.seq) + len(t.description) + 10 <= max_seq_len, it)
+    it = islice(it, 0, num_samples)
 
-MAX_SEQ_LEN = 2048
-NUM_SAMPLES = 25000
+    with gzip.open(write_to, 'wb') as f:
+        for sample in it:
+            data = f' > {sample.description} # {sample.seq}'
+            data = data.encode('utf-8')
+            f.write(data)
 
-it = SeqIO.parse('./uniref50.fasta', 'fasta')
-it = filter(lambda t: len(t.seq) + len(t.description) + 10 <= MAX_SEQ_LEN, it)
-it = islice(it, 0, NUM_SAMPLES)
-
-with gzip.open('./data/uniref50.sample.gz', 'wb') as f:
-    for sample in it:
-        data = f' > {sample.description} # {sample.seq}'
-        data = data.encode('utf-8')
-        f.write(data)
+if __name__ == '__main__':
+    main()
